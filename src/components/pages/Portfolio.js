00978fddd28950ec25640abfc62e3a0586a0a270
Haseebz1 <haseebkandore@hotmail.com>
2023-08-24 23:12:45 +0100

    1-5 portfolio pic and link added

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-let myPic = '/Assets/images/placeholder.webp';
-let wearCoPic = '/Assets/images/WearCo.png'
+let wearCoPic = '/Assets/images/WearCoSC.png';
+let noteTakerPic = '/Assets/images/NotetakerSC.png';
+let textEditorPic = '/Assets/images/textEditor-PWA.png';
+let weatherDashPic = '/Assets/images/weatherDashSc.png';
+let readmeGeneratorPic = '/Assets/images/readMeGenerator.png';
+let DailyPlanerPic ='/Assets/images/readMeGenerator.png'
 
 export default function Portfolio() {
   return (
@@ -21,7 +25,7 @@ export default function Portfolio() {
           <img
             width="175px"
             src={process.env.PUBLIC_URL + wearCoPic}
-            alt="Haseeb"
+            alt="WearCoPic"
           />
           <a href="https://github.com/cckinwest/Project2-WearCorp.git" target="_blank" rel="noopener noreferrer">
           <p>click here for code</p>
@@ -31,10 +35,12 @@ export default function Portfolio() {
         <p>Project 2</p>
           <img
             width="175px"
-            src={process.env.PUBLIC_URL + myPic}
-            alt="Haseeb"
+            src={process.env.PUBLIC_URL + noteTakerPic}
+            alt="NotetakerPic"
           />
+           <a href="https://github.com/Haseebz1/NoteTaker.git" target="_blank" rel="noopener noreferrer">
           <p>click here for code</p>
+          </a>
         </div>
       </div>
 
@@ -43,40 +49,48 @@ export default function Portfolio() {
         <p>Project 3</p>
           <img
             width="175px"
-            src={process.env.PUBLIC_URL + myPic}
-            alt="Haseeb"
+            src={process.env.PUBLIC_URL + textEditorPic}
+            alt="WearCoPic"
           />
+          <a href="https://github.com/Haseebz1/textEditor-PWA.git" target="_blank" rel="noopener noreferrer">
           <p>click here for code</p>
+          </a>
         </div>
         <div className="col-md-5">
         <p>Project 4</p>
           <img
             width="175px"
-            src={process.env.PUBLIC_URL + myPic}
-            alt="Haseeb"
+            src={process.env.PUBLIC_URL + weatherDashPic}
+            alt=" WeatherDash"
           />
+           <a href="https://github.com/Haseebz1/weatherDash.git" target="_blank" rel="noopener noreferrer">
           <p>click here for code</p>
+          </a>
         </div>
       </div>
 
       <div className="row">
         <div className="col-md-5">
-          <p>Project 5</p>
+        <p>Project 5</p>
           <img
             width="175px"
-            src={process.env.PUBLIC_URL + myPic}
-            alt="Haseeb"
+            src={process.env.PUBLIC_URL + readmeGeneratorPic}
+            alt="Readme Genrator"
           />
+          <a href="https://github.com/Haseebz1/ReadmeGenerator.git" target="_blank" rel="noopener noreferrer">
           <p>click here for code</p>
+          </a>
         </div>
         <div className="col-md-5">
-          <p>Project 6</p>
+        <p>Project 6</p>
           <img
             width="175px"
-            src={process.env.PUBLIC_URL + myPic}
-            alt="Haseeb"
+            src={process.env.PUBLIC_URL + noteTakerPic}
+            alt="DailyPlaner"
           />
+           <a href="https://github.com/Haseebz1/Daily-Planner.git" target="_blank" rel="noopener noreferrer">
           <p>click here for code</p>
+          </a>
         </div>
       </div>
 
