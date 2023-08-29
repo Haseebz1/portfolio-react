353b4a0f63805f83e9d238bec4e83e02cf6d8b05
Haseebz1 <haseebkandore@hotmail.com>
2023-08-29 17:02:18 +0100

    Aboutme back ground color changed and footer color and icon css style added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./footer.css";
 function Footer() {
   return (
     <footer className=" text-center footer">
-    <a
+      <a
         href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
         target="_blank"
         rel="noopener noreferrer"
@@ -12,7 +12,7 @@ function Footer() {
         <img
           src={process.env.PUBLIC_URL + "/Assets/images/linkedin-icon.png"}
           alt="linkedin-icon"
-          width="50px"
+          className="icon"
         />
       </a>
 
@@ -24,10 +24,9 @@ function Footer() {
         <img
           src={process.env.PUBLIC_URL + "/Assets/images/cv-icon1.png"}
           alt="cvicon"
-          width="50px"
+          className="icon"
         />
       </a>
-
 
       <a
         href="https://instagram.com/has_jatt?utm_source=qr&igshid=MzNINGNkZWQ4Mg%3D%3D"
@@ -37,10 +36,10 @@ function Footer() {
         <img
           src={process.env.PUBLIC_URL + "/Assets/images/insta-icon.jpg"}
           alt="cvicon"
-          width="50px"
+          className="icon"
         />
       </a>
-    
+
       <span>&copy; Copyright 2020 Haseeb Zubair</span>
     </footer>
   );
