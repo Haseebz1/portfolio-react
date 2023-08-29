1371bc323ea9116428bd728876a6d693ee3031a6
Haseebz1 <haseebkandore@hotmail.com>
2023-08-29 12:05:34 +0100

    added linkedin icon in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,7 @@ import "./footer.css";
 function Footer() {
     return (
       <footer className=" text-center footer">
+        <a href="url" _target="blank"  > <img src={process.env.PUBLIC_URL + "/Assets/images/linkedin-icon.png"} alt="linkedin con" width="50px"/> </a>
         <span>&copy; Copyright 2020 Haseeb Zubair</span>
       </footer>
     );
